7119ac26de12fd1febd0c9d180c1ffed230aeff0
Erick Humberto Cordova Gavilanes <erihucor@espol.edu.ec>
2019-02-27 22:41:31 -0500

    Primer tema

--- FP-1P-2016-2S.py
+++ FP-1P-2016-2S.py
@@ -2,3 +2,26 @@
 import numpy as np
 import random as rd
 #Tema 1
+
+L_vocales = ['a', 'e', 'i', 'o', 'u']
+L_consonantes = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+
+texto = input("Ingrese un texto: ").lower()
+contador_palabra = 0
+
+texto = texto.replace("."," ")
+
+L_pal = texto.split(" ")
+for palabra in L_pal:
+    contador_vocales = 0
+    contador_consonantes = 0
+    if palabra != "":#Caso especial cuando termina en punto el split crea un caracter vacio el cual debe ser validado
+        for letra in palabra:
+            if letra in L_vocales:
+                contador_vocales+=1
+            elif letra in L_consonantes:
+                contador_consonantes+=1
+        if contador_vocales==contador_consonantes:
+            contador_palabra+=1
+
+print("el texto tiene %d palabras"%contador_palabra)
